f67f688d0db71757162e8991a5627baf8c190eae
Tarik <tarikdavis@hotmail.co.uk>
2020-01-15 20:20:39 +0000

    Updates

---
+++ precache-manifest.6e1681e8474b30b2243232845cc44ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c712237df046188a5925c1d899511280",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "97ec543cc7c26791d661",
+    "url": "/spotify/static/css/main.e39d21ad.chunk.css"
+  },
+  {
+    "revision": "5bbbdd8dca870190fb03",
+    "url": "/spotify/static/js/2.0377b074.chunk.js"
+  },
+  {
+    "revision": "97ec543cc7c26791d661",
+    "url": "/spotify/static/js/main.c1a8c102.chunk.js"
+  },
+  {
+    "revision": "ad580f68e8b88251aec2",
+    "url": "/spotify/static/js/runtime~main.83d9da63.js"
+  }
+]);
